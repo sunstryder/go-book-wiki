2b56542d2974fba6b20053e164b5287b726b2bcc
Tony Sun <tsun@zendesk.com>
2019-11-20 10:40:52 +1100

    add a main function, update gitignore

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 // The Page struct describes how page data is stored in memory
 type Page struct {
@@ -15,11 +18,25 @@ func (p *Page) save() error {
 }
 
 // function local to this package, lowercase first letter to indicate that it's not exported.
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := ioutil.ReadFile(filename)
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 	return &Page{
 		Title: title,
 		Body:  body,
-	}
+	}, nil
+}
+
+func main() {
+	// create a new Page struct with the given data
+	p1 := &Page{Title: "The Name of the Wind", Body: []byte("It was a silence of three parts")}
+	//save it to a file
+	p1.save()
+	// load the contents of the file
+	p2, _ := loadPage("The Name of the Wind")
+	// spit out the body into console
+	fmt.Println(string(p2.Body))
 }
