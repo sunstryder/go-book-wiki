847183911ff6ffe32f4d83e00d3e42475aac779b
Tony Sun <tsun@zendesk.com>
2019-11-22 09:51:04 +1100

    rename main to wiki & add some routes and handlers

---
+++ wiki.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// The Page struct describes how page data is stored in memory
+type Page struct {
+	Title string
+	Body  []byte // this means "byte slice". Body is of this type because our io libraries use this type, not string
+}
+
+// saves Page body to a text file named "title.txt" This is a method and not a func because it ˙has a receiver
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600) // the 0600 is a octo literall indicating this file has read-write permission for current user only.
+}
+
+// function local to this package, lowercase first letter to indicate that it's not exported.
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{
+		Title: title,
+		Body:  body,
+	}, nil
+}
+
+// our handlers all take in responseWriter and request pointer params
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", editHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
